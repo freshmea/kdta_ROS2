19bd0eb3d86f19277b88335f57f91e79272e0000
freshmea <freshmea@naver.com>
2024-07-23 14:22:16 +0900

    aa

--- opencv/first/helloworld.cpp
+++ opencv/first/helloworld.cpp
@@ -2,8 +2,14 @@
 #include <iostream>
 #include <string>
 
+std::string folder = "/home/aa/kdta_ROS2/opencv/first/";
+
 int main()
 {
     std::cout << "hello, world" << std::endl;
+    cv::Mat img;
+    img = cv::imread(folder + "lena.bmp");
+    cv::imshow("image", img);
+    cv::waitKey(0);
     return 0;
 }
