106384140dd404ac65afefb5b4ddfe2cc0ebdf32
freshmea <freshmea@naver.com>
2024-07-26 09:48:35 +0900

    affine

---
+++ opencv/ch08/affine.cpp
@@ -0,0 +1,36 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+String folder = "/home/aa/kdta_ROS2/opencv/data/";
+
+int main()
+{
+    Mat src = imread(folder + "tekapo.bmp");
+    Point2f srcPts[3], dstPts[3];
+    // srcPts[0] = Point2f(0, 0);
+    // srcPts[1] = Point2f(1, 0);
+    // srcPts[2] = Point2f(1, 1);
+    // dstPts[0] = Point2f(0, 0);
+    // dstPts[1] = Point2f(2, 0);
+    // dstPts[2] = Point2f(2, 2);
+    srcPts[0] = Point2f(0, 0);
+    srcPts[1] = Point2f(src.cols - 1, 0);
+    srcPts[2] = Point2f(src.cols - 1, src.rows - 1);
+    dstPts[0] = Point2f(50, 50);
+    dstPts[1] = Point2f(src.cols - 1 + 50, 50);
+    dstPts[2] = Point2f(src.cols - 1 + 50, src.rows - 1 + 50);
+
+    Mat M = getAffineTransform(srcPts, dstPts);
+    cout << M << endl;
+    Mat dst;
+
+    Size sz1 = src.size();
+    warpAffine(src, dst, M, sz1);
+
+    imshow("src", src);
+    imshow("dst", dst);
+    waitKey(0);
+    return 0;
+}
