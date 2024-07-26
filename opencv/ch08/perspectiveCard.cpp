fe367e2e3de0dbfa48236058a4c4f405b0aa33f6
freshmea <freshmea@naver.com>
2024-07-26 11:12:07 +0900

    card

--- opencv/ch08/perspectiveCard.cpp
+++ opencv/ch08/perspectiveCard.cpp
@@ -8,12 +8,11 @@ void onMouse(int event, int x, int y, int flags, void *data);
 String folder = "/home/aa/kdta_ROS2/opencv/data/";
 
 Mat src;
-bool flag = false;
 Point2f srcPts[4], dstPts[4];
 
 int main()
 {
-    Mat img = imread(folder + "card.bmp");
+    src = imread(folder + "card.bmp");
 
     namedWindow("src");
     setMouseCallback("src", onMouse);
@@ -33,6 +32,22 @@ void onMouse(int event, int x, int y, int flags, void *data)
         if (cnt < 4)
         {
             srcPts[cnt++] = Point2f(x, y);
+            circle(src, Point(x, y), 5, Scalar(0, 0, 255), -1);
+            imshow("src", src);
+            if (cnt == 4)
+            {
+                int w = 200, h = 300;
+                dstPts[0] = Point2f(0, 0);
+                dstPts[1] = Point2f(w - 1, 0);
+                dstPts[2] = Point2f(w - 1, h - 1);
+                dstPts[3] = Point2f(0, h - 1);
+
+                Mat M = getPerspectiveTransform(srcPts, dstPts);
+                Mat dst;
+                warpPerspective(src, dst, M, Size(w, h));
+                imshow("dst", dst);
+                cnt = 0;
+            }
         }
     }
 }
