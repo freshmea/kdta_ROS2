78d564c3d51b8f3f7ef30e33912964d1ba6675a0
freshmea <freshmea@naver.com>
2024-07-31 17:08:30 +0900

    aa

---
+++ topic_final/src/mpub.cpp
@@ -0,0 +1,40 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+class MessagePub : public rclcpp::Node
+{
+public:
+    MessagePub()
+        : Node("mpub"), _i(0)
+    {
+        auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+        _pub = this->create_publisher<std_msgs::msg::String>("helloworld", qos_profile);
+        _timer = this->create_wall_timer(1s, std::bind(&MessagePub::publish_helloworld_msg, this));
+    }
+
+private:
+    int _i;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub;
+    rclcpp::TimerBase::SharedPtr _timer;
+    void publish_helloworld_msg()
+    {
+        auto msg = std_msgs::msg::String();
+        msg.data = "from mpub!" + to_string(_i);
+        _pub->publish(msg);
+        _i++;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MessagePub>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ topic_final/src/tpub.cpp
@@ -0,0 +1,43 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/header.hpp"
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+class TimePub : public rclcpp::Node
+{
+public:
+    TimePub()
+        : Node("tpub"), _i(0)
+    {
+        auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+        _pub = this->create_publisher<std_msgs::msg::Header>("tpub", qos_profile);
+        _timer = this->create_wall_timer(1s, std::bind(&TimePub::publish_time_msg, this));
+        _clock = rclcpp::Clock(RCL_ROS_TIME);
+    }
+
+private:
+    int _i;
+    rclcpp::Publisher<std_msgs::msg::Header>::SharedPtr _pub;
+    rclcpp::TimerBase::SharedPtr _timer;
+    rclcpp::Clock _clock;
+    void publish_time_msg()
+    {
+        auto msg = std_msgs::msg::Header();
+        msg.stamp = _clock.now();
+        msg.frame_id = "tpub" + to_string(_i);
+        _pub->publish(msg);
+        _i++;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<TimePub>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
