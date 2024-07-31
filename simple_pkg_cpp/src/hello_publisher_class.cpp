20370965cf25b58dee63fb75deb7d177a0658f26
freshmea <freshmea@naver.com>
2024-07-31 10:01:56 +0900

    aa

--- simple_pkg_cpp/src/hello_publisher_class.cpp
+++ simple_pkg_cpp/src/hello_publisher_class.cpp
@@ -31,9 +31,9 @@ private:
     }
 };
 
-int main()
+int main(int argc, char *argv[])
 {
-    rclcpp::init(0, nullptr);
+    rclcpp::init(argc, argv);
     auto node = std::make_shared<HellowPublisher>();
     rclcpp::spin(node);
     rclcpp::shutdown();
