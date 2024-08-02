0c395783099eee9dbffe2c4e8e18cba3fb0f5a0e
freshmea <freshmea@naver.com>
2024-08-02 16:35:49 +0900

    launch param

--- topic_final/launch/tmpubsub.launch.py
+++ topic_final/launch/tmpubsub.launch.py
@@ -1,9 +1,19 @@
 # tmpubsub.launch.py
+import os
+
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    param_dir = LaunchConfiguration('param_dir', default=os.path.join(
+        get_package_share_directory('topic_final'),
+        'param',
+        'turtlesim.yaml'
+        ))
+    
     return LaunchDescription([
         Node(package='topic_final',
              executable='mpub',
@@ -27,5 +37,6 @@ def generate_launch_description():
              ),
         Node(package='turtlesim',
              executable='turtlesim_node',
-             output='screen',)
+             output='screen',
+             parameters=[param_dir])
         ])
