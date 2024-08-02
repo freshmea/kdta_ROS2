47d52896b1cf452ca8fca21c66fed93859d4e98d
freshmea <freshmea@naver.com>
2024-08-02 14:52:23 +0900

    aa

--- simple_pkg_cpp/src/simple_service_client.cpp
+++ simple_pkg_cpp/src/simple_service_client.cpp
@@ -14,20 +14,6 @@ public:
         : Node("addTwoInt_client"), _a(40), _b(74)
     {
         _client = create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
-        _timer = this->create_wall_timer(5s, std::bind(&SimpleServiceClient::send_request, this));
-        RCLCPP_INFO(get_logger(), "Service Server Ready to add two ints.");
-    }
-
-private:
-    int _a;
-    int _b;
-    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr _client;
-    rclcpp::TimerBase::SharedPtr _timer;
-    void send_request()
-    {
-        auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-        request->a = _a;
-        request->b = _b;
         while (!_client->wait_for_service(1s)) // 서버연결
         {
             if (!rclcpp::ok())
@@ -36,7 +22,16 @@ private:
             }
             RCLCPP_INFO(get_logger(), "service not available, waiting again...");
         }
+        RCLCPP_INFO(get_logger(), "Service is ready!");
+    }
+    void send_request()
+    {
+        RCLCPP_INFO(get_logger(), "Sending request...");
+        auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+        request->a = _a;
+        request->b = _b;
         auto result = _client->async_send_request(request); // 비동기 요청
+        RCLCPP_INFO(get_logger(), "After async...");
         if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == rclcpp::FutureReturnCode::SUCCESS)
         {
             RCLCPP_INFO(get_logger(), "Result of %d + %d = %ld", _a, _b, result.get()->sum);
@@ -46,13 +41,18 @@ private:
             RCLCPP_INFO(get_logger(), "Service call failed!");
         }
     };
+
+private:
+    int _a;
+    int _b;
+    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr _client;
 };
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<SimpleServiceClient>();
-    rclcpp::spin(node);
+    node->send_request();
     rclcpp::shutdown();
     return 0;
 }
