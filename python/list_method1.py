dada8a97e06ae34287481f0e92eacde9ebab7c37
freshmea <freshmea@naver.com>
2024-08-28 15:20:27 +0900

    aa

--- python/list_method1.py
+++ python/list_method1.py
@@ -6,14 +6,14 @@ def main():
     print(li1 * 3)
     print(len(li2))
     print(li2.__len__())
-    
+
     print("extend: ")
     print(li1)
     li2.append("kim")
     print(li2)
     li2.insert(4, "Jo")
     print(li2)
-    
+
 
 if __name__ == "__main__":
     main()
